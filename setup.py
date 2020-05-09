e0d69446619345605f8de6a2c5c04421d4b80aca
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-09 16:41:47 +0100

    bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name="morphapi",
-    version="0.0.2",
+    version="0.0.3",
     description="A lightweight python package to download neuronal morphologies",
     long_description=long_description,
     long_description_content_type="text/markdown",
