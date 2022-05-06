51b87e96d54d34a520a564cf81825f3ec188e65f
Adrien Berchet <adrien.berchet@epfl.ch>
2022-05-06 15:48:58 +0200

    Fix coverage

--- morphapi/paths_manager.py
+++ morphapi/paths_manager.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 
 """
@@ -15,17 +14,16 @@ default_paths = dict(
     meshes_cache="Data/meshes_cache",
     # Other
     mouse_connectivity_cache="Data/mouse_connectivity_cache",
-    mpin_morphology="Data/mpin_morphology"
+    mpin_morphology="Data/mpin_morphology",
 )
 
 
 class Paths:
-
     def __init__(self, base_dir=None, **kwargs):
         """
-        Parses a YAML file to get data folders paths. Stores paths to a number of folders used throughtout morphapi. 
-        
-        :param base_dir: str with path to directory to use to save data. If none the user's base directiry is used. 
+        Parses a YAML file to get data folders paths. Stores paths to a number of folders used throughtout morphapi.
+
+        :param base_dir: str with path to directory to use to save data. If none the user's base directiry is used.
         :param kwargs: use the name of a folder as key and a path as argument to specify the path of individual subfolders
         """
         # Get and make base directory
@@ -40,8 +38,7 @@ class Paths:
         for fld_name, folder in default_paths.items():
             # Check if user provided a path for this folder, otherwise use default
 
-            path = self.base_dir / kwargs.pop(fld_name,
-                                              folder)
+            path = self.base_dir / kwargs.pop(fld_name, folder)
 
             # Create folder if it doesn't exist:
             path.mkdir(parents=True, exist_ok=True)
